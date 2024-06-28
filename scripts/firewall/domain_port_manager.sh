4d8883f6ae18783b3846807220fd909377f5f53f
SilverIceKey <z516798599@gmail.com>
2024-06-28 15:31:30 +0800

    更新脚本查看bug是否修复

--- scripts/firewall/domain_port_manager.sh
+++ scripts/firewall/domain_port_manager.sh
@@ -38,7 +38,7 @@ add_config() {
     read -p "请输入端口: " port
     ip=$(dig +short $domain | tail -n1)
 
-    if [ -z "$ip" ];然then
+    if [ -z "$ip" ]; then
         echo "无法解析域名 $domain"
         return
     fi
@@ -52,7 +52,7 @@ add_config() {
 # 删除配置
 remove_config() {
     read -p "请输入要删除的域名: " domain
-    if [ ! -f $CONFIG_FILE ];然then
+    if [ ! -f $CONFIG_FILE ]; then
         echo "无配置"
         return
     fi
@@ -77,7 +77,7 @@ reload_iptables() {
 
 # 更新IP地址
 update_ips() {
-    if [ ! -f $CONFIG_FILE ];然then
+    if [ ! -f $CONFIG_FILE ]; then
         return
     fi
 
@@ -87,7 +87,7 @@ update_ips() {
         old_ip=$(echo $line | awk '{print $3}')
         new_ip=$(dig +short $domain | tail -n1)
 
-        if [ "$old_ip" != "$new_ip" ];然then
+        if [ "$old_ip" != "$new_ip" ]; then
             echo "更新 $domain:$port -> $new_ip"
             sudo iptables -D $IPTABLES_CHAIN -p tcp --dport $port -s $old_ip -j ACCEPT
             sudo iptables -A $IPTABLES_CHAIN -p tcp --dport $port -s $new_ip -j ACCEPT
@@ -110,7 +110,7 @@ modify_interval() {
 main_menu() {
     PS3="请选择操作: "
     options=("列出配置" "新增配置" "删除配置" "修改更新间隔" "退出")
-    select opt in "${options[@]}";然do
+    select opt in "${options[@]}"; do
         case $opt in
             "列出配置")
                 list_config
